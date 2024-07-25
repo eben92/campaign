2286d3d28a7f383e2557006c4666d4f50a3ead23
eben96 <pappy.rhutmannjnrolege@gmail.com>
2024-07-25 06:54:40 +0000

    first commit

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -7,11 +7,24 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(middleware.CleanPath)
+	r.Use(middleware.AllowContentType("application/json", "application/x-www-form-urlencoded", "multipart/form-data", "text/plain", "text/html"))
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "Accept-Encoding"},
+		AllowCredentials: true,
+	}))
+
+	r.Route("/api", func(gr chi.Router) {
+		gr.Get("/", s.HelloWorldHandler)
+	})
 
 	r.Get("/", s.HelloWorldHandler)
 
